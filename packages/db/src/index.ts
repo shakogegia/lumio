4e8f78336619d7120222d4f665a7c226caebcb98
Shalva Gegia <shalva.gegia@gmail.com>
2026-06-17 18:54:11 +0200

    feat: add Prisma schema and client singleton

---
+++ packages/db/src/index.ts
@@ -0,0 +1,4 @@
+export { prisma } from "./client.js";
+export * from "./mappers.js";
+export { PhotoSource } from "@prisma/client";
+export type { Photo, Album, AlbumPhoto, Prisma, PrismaClient } from "@prisma/client";
